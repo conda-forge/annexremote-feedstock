942243fa1ff101d75115d759c94e177f14e6e37f
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2019-01-09 22:23:10 -0500

    added missing git annex init

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -4,6 +4,7 @@ set -eu -o pipefail -x
 
 nosetests -v
 python -c "from annexremote import Master, SpecialRemote, RemoteError"
+git-annex-remote-directory < /dev/null | grep VERSION
 
 CURDIR=$(pwd)
 REMOTE_DIR=$CURDIR/remote
@@ -12,6 +13,7 @@ mkdir -p $REMOTE_DIR
 mkdir -p $REPO_DIR
 pushd $REPO_DIR
 git init
+git annex init
 git annex initremote directory_remote type=external externaltype=directory encryption=none directory=$REMOTE_DIR
 git annex testremote directory_remote
 popd
