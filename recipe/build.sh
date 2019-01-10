bbeb2afbf766800dfeda813406785cc01a0bdb64
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2019-01-10 14:21:22 -0500

    added testing using git-annex testremote

---
+++ recipe/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eu -o pipefail -x
+
+nosetests --verbose
+$PYTHON -m pip install . --no-deps --ignore-installed -vv
+mkdir -p $PREFIX/bin
+cp examples/git-annex-remote-directory $PREFIX/bin/
+chmod u+x $PREFIX/bin/git-annex-remote-directory
