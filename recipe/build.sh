d7cb60a8ac6a8adf5fa092875726c85d479094f8
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2019-01-09 22:18:26 -0500

    added creation of repo dir

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,5 +3,6 @@
 set -eu -o pipefail
 
 $PYTHON -m pip install . --no-deps --ignore-installed -vv
+mkdir -p $PREFIX/bin
 cp examples/git-annex-remote-directory $PREFIX/bin/
 chmod u+x $PREFIX/bin/git-annex-remote-directory
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -9,6 +9,7 @@ CURDIR=$(pwd)
 REMOTE_DIR=$CURDIR/remote
 REPO_DIR=$CURDIR/repo
 mkdir -p $REMOTE_DIR
+mkdir -p $REPO_DIR
 pushd $REPO_DIR
 git init
 git annex initremote directory_remote type=external externaltype=directory encryption=none directory=$REMOTE_DIR
