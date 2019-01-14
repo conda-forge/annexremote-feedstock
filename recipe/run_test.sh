f02217ed4279250de22233045c1190c27628f981
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2019-01-14 11:27:20 -0500

    enable macos build

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -6,16 +6,21 @@ nosetests --verbose
 python -c "from annexremote import Master, SpecialRemote, RemoteError"
 git-annex-remote-directory < /dev/null | grep VERSION
 
-CURDIR=$(pwd)
-REMOTE_DIR=$CURDIR/remote
-REPO_DIR=$CURDIR/repo
-mkdir -p $REMOTE_DIR
-mkdir -p $REPO_DIR
-pushd $REPO_DIR
-git init
-git config user.email "someuser@gmail.com"
-git config user.name "Some User"
-git annex init
-git annex initremote directory_remote type=external externaltype=directory encryption=none directory=$REMOTE_DIR
-git annex testremote directory_remote
-popd
+# If git-annex is available, run tests using git-annex-testremote
+if [[ $(type -P "git-annex") ]]
+then
+    CURDIR=$(pwd)
+    REMOTE_DIR=$CURDIR/remote
+    REPO_DIR=$CURDIR/repo
+    mkdir -p $REMOTE_DIR
+    mkdir -p $REPO_DIR
+    pushd $REPO_DIR
+    git init
+    git config user.email "someuser@gmail.com"
+    git config user.name "Some User"
+    git annex init
+    git annex initremote directory_remote type=external externaltype=directory encryption=none directory=$REMOTE_DIR
+    git annex testremote directory_remote
+    popd
+fi
+
