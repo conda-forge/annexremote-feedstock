1b2d3760edcb5016e15965f1e7fd2840f5ad2e72
Ilya Shlyakhter <ilya_shl@alum.mit.edu>
2019-03-21 15:53:39 -0400

    MNT: Re-rendered with conda-build 3.17.8, conda-smithy 3.3.2, and conda-forge-pinning 2019.03.17; bumped build to 1001 (#14)
    MNT: Re-rendered with conda-build 3.17.8, conda-smithy 3.3.2, and conda-forge-pinning 2019.03.17; bumped build to 1001
    <!--
    Thank you for pull request.
    Below are a few things we ask you kindly to self-check before getting a review. Remove checks that are not relevant.
    -->
    Checklist
    * [X] Used a fork of the feedstock to propose changes
    * [X] Bumped the build number (if the version is unchanged)
    * [ ] Reset the build number to `0` (if the version changed)
    * [X] [Re-rendered]( https://conda-forge.org/docs/conda_smithy.html#how-to-re-render ) with the latest `conda-smithy` (Use the phrase <code>@<space/>conda-forge-admin, please rerender</code> in a comment in this PR for automated rerendering)
    * [X] Ensured the license file is being packaged.
    
    <!--
    Please note any issues this fixes using [closing keywords]( https://help.github.com/articles/closing-issues-using-keywords/ ):
    -->
    
    <!--
    Please add any other relevant info below:
    -->

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,6 @@
 
 set -eu -o pipefail -x
 
-pwd
-ls -altR
-
-$PYTHON -m pip install . --no-deps --ignore-installed -vv
 mkdir -p $PREFIX/bin
+$PYTHON -m pip install . --no-deps --ignore-installed -vv
+
